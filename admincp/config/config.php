e5c4dcc056e8642101678b6061e81c056e711e3f
21012064-BuiThiNgocHuyen <124747527+21012064-BuiThiNgocHuyen@users.noreply.github.com>
2024-06-19 13:55:55 +0700

    Delete admincp directory

--- admincp/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost","root","","website");
-
-// Check connection
-if ($mysqli->connect_errno) {
-  echo "Lỗi kết nối " . $mysqli->connect_error;
-  exit();
-}
-?> 

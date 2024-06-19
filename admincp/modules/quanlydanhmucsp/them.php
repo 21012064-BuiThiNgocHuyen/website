ea4accadb531ce9c4c47e77cdf951cf1c79b7217
21012064-BuiThiNgocHuyen <124747527+21012064-BuiThiNgocHuyen@users.noreply.github.com>
2024-06-19 23:04:14 +0700

    Update them.php

--- admincp/modules/quanlydanhmucsp/them.php
+++ admincp/modules/quanlydanhmucsp/them.php
@@ -3,11 +3,11 @@
   <form action="modules/quanlydanhmucsp/xuly.php" method="POST">
   <tr>
     <td>Tên danh mục</td>
-    <td><input type="text" name="tendanhmuc"></td>
+    <td><input type="text" name="tendanhmuc" required></td>
   </tr>
   <tr>
     <td>Thứ tự</td>
-    <td><input type="text" name="thutu"></td>
+    <td><input type="text" name="thutu"  required></td>
   </tr>
   <tr>
     <td colspan="2"><input type="submit" name="themdanhmuc" value="Thêm danh mục sản phẩm"></td>
